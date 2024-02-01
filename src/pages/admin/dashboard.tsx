b4ea126654711be83c86fc189421d53a72acfb35
Leonardo Duarte <hintl3rgames2015@gmail.com>
2024-02-01 13:54:22 -0300

    adicionando label no barCharts

--- src/pages/admin/dashboard.tsx
+++ src/pages/admin/dashboard.tsx
@@ -10,9 +10,10 @@ import { useSelector } from 'react-redux';
 import { RootState } from '../../redux/store';
 import { Skeleton } from '../../components/Loader';
 import { Navigate } from 'react-router-dom';
+import { getLastMonsths } from '../../utils/features';
 const userImg =
   'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSJxA5cTf-5dh5Eusm0puHbvAhOrCRPtckzjA&usqp';
-
+const { last6Months: months } = getLastMonsths();
 const Dashboard = () => {
   const { user } = useSelector((state: RootState) => state.userReducer);
   const { isLoading, data, isError } = useStatsQuery(user?._id!);
@@ -64,6 +65,7 @@ const Dashboard = () => {
               <div className="revenue-chart">
                 <h2>Revenue & Transaction</h2>
                 <BarChart
+                  labels={months}
                   data_1={stats.chart.revenue}
                   data_2={stats.chart.order}
                   title_1="Revenue"
